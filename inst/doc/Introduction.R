9a9f4e5f16454673b760ba78a4570dc59cf72375
Floid R. Gilbert <floid.r.gilbert@gmail.com>
2020-06-04 19:50:02 +0000

    version 0.1.4

--- inst/doc/Introduction.R
+++ inst/doc/Introduction.R
@@ -1,28 +1,28 @@
-## ----setup, include=FALSE------------------------------------------------
+## ----setup, include=FALSE-----------------------------------------------------
 library("pander")
 knitr::opts_chunk$set(echo = TRUE)
 # The conversion tables have been copied from mapping.xlsx and converted to R character matrices.
 rdata.tables <- "introduction-tables.RData"
 load(rdata.tables)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 m1 <- matrix(1:6, 2, 3)
 m1
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library("jdx")
 o <- convertToJava(m1)
 getJavaClassName(o)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 o <- rJava::.jcast(o, "[Ljava/lang/Object;")
 rJava::.jcall("java/util/Arrays", "S", "deepToString", o)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 m2 <- convertToR(o)
 identical(m1, m2)
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  convertToJava(
 #    value,
 #    length.one.vector.as.array = FALSE,
@@ -32,7 +32,7 @@ identical(m1, m2)
 #    coerce.factors = TRUE
 #  )
 
-## ----results='asis', echo=FALSE------------------------------------------
+## ----results='asis', echo=FALSE-----------------------------------------------
 # load(rdata.tables)
 # Highlight and copy the appropriate section in mapping.xlsx before executing the next line
 # r2j.length.one.vector.scalars.as.objects.false <- mt(9, 4, what = character(), write.to.clipboard = FALSE)
@@ -47,7 +47,7 @@ pander::pander(
   , emphasize.strong.rows = 1
 )
 
-## ----results='asis', echo=FALSE------------------------------------------
+## ----results='asis', echo=FALSE-----------------------------------------------
 # load(rdata.tables)
 # Highlight and copy the appropriate section in mapping.xlsx before executing the next line
 # r2j.length.one.vector.scalars.as.objects.true <- mt(9, 4, what = character(), write.to.clipboard = FALSE)
@@ -62,7 +62,7 @@ pander::pander(
   , emphasize.strong.rows = 1
 )
 
-## ----results='asis', echo=FALSE------------------------------------------
+## ----results='asis', echo=FALSE-----------------------------------------------
 # load(rdata.tables)
 # Highlight and copy the appropriate section in mapping.xlsx before executing the next line
 # r2j.vectors <- mt(9, 4, what = character(), write.to.clipboard = FALSE)
@@ -77,7 +77,7 @@ pander::pander(
   , emphasize.strong.rows = 1
 )
 
-## ----results='asis', echo=FALSE------------------------------------------
+## ----results='asis', echo=FALSE-----------------------------------------------
 # load(rdata.tables)
 # Highlight and copy the appropriate section in mapping.xlsx before executing the next line
 # r2j.arrays <- mt(9, 4, what = character(), write.to.clipboard = FALSE)
@@ -92,14 +92,14 @@ pander::pander(
   , emphasize.strong.rows = 1
 )
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  convertToR(
 #    value,
 #    strings.as.factors = NULL,
 #    array.order = "row-major"
 #  )
 
-## ----results='asis', echo=FALSE------------------------------------------
+## ----results='asis', echo=FALSE-----------------------------------------------
 # load(rdata.tables)
 # Highlight and copy the appropriate section in mapping.xlsx before executing the next line
 # j2r.scalars <- mt(12, 4, what = character(), write.to.clipboard = FALSE)
@@ -114,7 +114,7 @@ pander::pander(
   , emphasize.strong.rows = 1
 )
 
-## ----results='asis', echo=FALSE------------------------------------------
+## ----results='asis', echo=FALSE-----------------------------------------------
 # load(rdata.tables)
 # Highlight and copy the appropriate section in mapping.xlsx before executing the next line
 # j2r.arrays <- mt(13, 4, what = character(), write.to.clipboard = FALSE)
@@ -129,7 +129,7 @@ pander::pander(
   , emphasize.strong.rows = 1
 )
 
-## ----results='asis', echo=FALSE------------------------------------------
+## ----results='asis', echo=FALSE-----------------------------------------------
 # load(rdata.tables)
 # Highlight and copy the appropriate section in mapping.xlsx before executing the next line
 # j2r.collections <- mt(10, 4, what = character(), write.to.clipboard = FALSE)
@@ -144,56 +144,56 @@ pander::pander(
   , emphasize.strong.rows = 1
 )
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lst <- list(1.1, 2L, as.raw(3))
 (o <- convertToJava(lst))
 (r <- convertToR(o))
 class(r)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lst <- list(1.1, "a")
 (o <- convertToJava(lst))
 (r <- convertToR(o))
 class(r)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lst <- list(c(1, 2), c(3L, 4L))
 (o <- convertToJava(lst, array.order = "column-major"))
 (r <- convertToR(o, array.order = "column-major"))
 class(r)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # This example results in a three-dimensional array.
 lst <- list(matrix(1:4, 2, 2), matrix(5:8, 2, 2))
 (o <- convertToJava(lst, array.order = "column-major"))
 convertToR(o, array.order = "column-major")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lst <- list(list(1))
 convertToR(convertToJava(lst))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lst <- list(
   list(field1 = "a", field2 = 1),
   list(field1 = "b", field2 = 2)
 )
 convertToR(convertToJava(lst))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lst <- list(a = 1.1, b = 2L, c = as.raw(3))
 convertToR(convertToJava(lst))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lst <- list(col1 = 1:3, col2 = letters[1:3])
 convertToR(convertToJava(lst))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 lst <- list(col1 = 1, col2 = "a")
 convertToR(convertToJava(lst))
 lst <- list(col1 = 1, col2 = "a")
 convertToR(convertToJava(lst, length.one.vector.as.array = TRUE))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 df <- data.frame(col1 = 1:3)
 convertToR(convertToJava(df, data.frame.row.major = FALSE))
 
